c775dd882c3c5ff82d5a4d890542985d684fc561
yong <yonghwani@hanmail.net>
2020-01-16 01:39:58 +0900

    sprite

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp');
 var iconfont = require('gulp-iconfont');
 var consolidate = require('gulp-consolidate');
  
+
 gulp.task('Iconfont', function(done){
   var iconStream = gulp.src(['icons/*.svg'])
    .pipe(iconfont({
@@ -49,5 +50,37 @@ gulp.task('Iconfont', function(done){
   ], done);
 });
 
+//-------------------------------------------------------------------------
+
+var buffer = require('vinyl-buffer');
+var csso = require('gulp-csso');
+var imagemin = require('gulp-imagemin');
+var merge = require('merge-stream');
+ 
+var spritesmith = require('gulp.spritesmith');
+ 
+gulp.task('sprite', function () {
+  // Generate our spritesheet
+  var spriteData = gulp.src('sprite-images/*.png').pipe(spritesmith({
+    imgName: 'sprite.png',
+    cssName: 'sprite.css'
+  }));
+ 
+  // Pipe image stream through image optimizer and onto disk
+  var imgStream = spriteData.img
+    // DEV: We must buffer our stream into a Buffer for `imagemin`
+    .pipe(buffer())
+    .pipe(imagemin())
+    .pipe(gulp.dest('images/'));
+ 
+  // Pipe CSS stream through CSS optimizer and onto disk
+  var cssStream = spriteData.css
+    //.pipe(csso())
+    .pipe(gulp.dest('css/'));
+ 
+  // Return a merged stream to handle both `end` events
+  return merge(imgStream, cssStream);
+});
+
 // 기본 구동 task
-gulp.task('default', ['Iconfont']);
+gulp.task('default', ['Iconfont', 'sprite']);
